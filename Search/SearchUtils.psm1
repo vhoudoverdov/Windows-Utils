970f136e525e4f9a8e2fdbc7be2ab3d712c563e0
Vasken Houdoverdov <vhcomptech@gmail.com>
2018-02-03 20:14:25 -0800

    Search utils and WMI wrapper

---
+++ Search/SearchUtils.psm1
@@ -0,0 +1,65 @@
+﻿<# 
+.SYNOPSIS 
+    Wrapper for Get-ChildItem that provides a simplified interface for recursively searching a directory for filenames.
+.Example 
+    Find-File "C:\" "canary"
+.Example
+    Find-File "C:\" "Canary" -CaseSensitive
+#>  
+
+function Find-File 
+{
+[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [String]$Path,
+   [Parameter(Mandatory=$True,Position=2)]
+   [String]$Filename,
+   [Switch]$CaseSensitive)
+
+   if(!$(Test-Path $Path)){throw "The specified path wasn't found:  $Path"}
+
+   Switch ($CaseSensitive){
+        $True { 
+                Get-ChildItem -Recurse $Path -ErrorAction SilentlyContinue | % {
+                    if ($_.BaseName -cmatch $Filename){$_.FullName}}
+              }
+        $False{
+                Get-ChildItem -Recurse $Path -ErrorAction SilentlyContinue | % {
+                    if ($_.BaseName -match $Filename){$_.FullName}}
+              }
+                      }
+}
+
+
+<# 
+.SYNOPSIS 
+    Wrapper for Get-ChildItem that provides a simplified interface for recursively searching a path for files which contain a specified string.
+.Example 
+    Find-ContentPattern "C:\" "canary"
+.Example
+    Find-ContentPattern "C:\" "the quick brown fox" -CaseSensitive
+#>
+function Find-ContentPattern {
+[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [String]$Path,
+   [Parameter(Mandatory=$True,Position=2)]
+   [String]$Pattern,
+   [Switch]$CaseSensitive)
+
+   if(!$(Test-Path $Path)){throw "The specified path wasn't found:  $Path"}
+
+   Switch ($CaseSensitive){
+        $True {
+                Get-ChildItem -Recurse $Path -ErrorAction SilentlyContinue | % {
+                   if ( -not $_.psiscontainer -and (Select-String -Case $Pattern $_ -ea SilentlyContinue )) {$_} }
+              }
+        $False{
+                Get-ChildItem -Recurse $Path -ErrorAction SilentlyContinue | % {
+                   if ( -not $_.psiscontainer -and (Select-String $Pattern $_ -ea SilentlyContinue)) {$_} }
+              }
+                      }
+} 
+Export-ModuleMember *
---
+++ TemperatureUtils/TemperatureUtils.psm1
@@ -0,0 +1,252 @@
+﻿<#
+.Synopsis
+Get-CurrentCPUTemperature uses the MSAcpi_ThermalZoneTemperature class to return the current CPU temperature.
+
+.Parameter Celsius
+Return current temperature as Celsius
+
+.Parameter Fahrenheit
+Return current temperature as Fahrenheit
+
+.Parameter Kelvin
+Return current temperature as Kelvin
+
+.Description
+Get-CurrentCPUTemperature uses the MSAcpi_ThermalZoneTemperature class to return the current CPU temperature.  Specify temperature unit using the parameters -Celsius, -Fahrenheit, and/or -Kelvin.  
+
+.Example
+Get-CurrentCPUTemperature -Celsius
+
+.Example
+Get-CurrentCPUTemperature -Fahrenheit
+
+.Example
+Get-CurrentCPUTemperature -Kelvin
+
+.Example
+Get-CurrentCPUTemperature -Celsius -Kelvin -Fahrenheit
+#>
+
+function Get-CurrentCPUTemperature
+{
+    [CmdletBinding()]
+    param(
+        [Switch]$Celsius,
+        [Switch]$Fahrenheit,
+        [Switch]$Kelvin
+        )
+    begin 
+        {
+        $WmiBase = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+        $WmiBase = $($WmiBase.CurrentTemperature/10)
+        $Bucket = @();
+        }
+    process
+        {
+        if(!$Celsius -and !$Kelvin -and !$Fahrenheit){$Fahrenheit=$True}
+        if($Celsius)
+            {
+            $c = $(($WmiBase)-273.15)
+            $Bucket += "$c" + [char]0x00B0 + " Celsius"
+            }
+        if($Fahrenheit)
+            {
+            $f = $((($WmiBase)-273.15) * (9/5) + 32)
+            $Bucket += "$f" + [char]0x00B0 +" Fahrenheit"
+            }
+        if($Kelvin)
+            {
+            $k = $WmiBase
+            $Bucket += "$k" + [char]0x00B0 +" Kelvin"
+            }
+        }
+    end
+       {return $Bucket}
+}
+
+<#
+.Synopsis
+Get-CriticalTripPoint uses the MSAcpi_ThermalZoneTemperature class to return the CPU temperature that triggers system cooling to begin.
+
+.Parameter Celsius
+Return the critical tripping point as Celsius
+
+.Parameter Fahrenheit
+Return the critical tripping point as Fahrenheit
+
+.Parameter Kelvin
+Return the critical tripping point as Kelvin
+
+.Description
+Get-CriticalTripPoint uses the MSAcpi_ThermalZoneTemperature class to return the CPU temperature that triggers system cooling to begin.  Specify temperature unit using the parameters -Celsius, -Fahrenheit, and/or -Kelvin.
+
+.Example
+Get-CriticalTripPoint -Celsius
+
+.Example
+Get-CriticalTripPoint -Fahrenheit
+
+.Example
+Get-CriticalTripPoint -Kelvin
+
+.Example
+Get-CriticalTripPoint -Celsius -Kelvin -Fahrenheit
+#>
+
+function Get-CriticalTripPoint 
+{
+    [CmdletBinding()]
+    param(
+        [Switch]$Celsius,
+        [Switch]$Fahrenheit,
+        [Switch]$Kelvin
+        )
+    begin 
+        {
+        $WmiBase = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+        $WmiBase = $($WmiBase.CriticalTripPoint/10)
+        $Bucket = @();
+        }
+    process
+        {
+        if(!$Celsius -and !$Kelvin -and !$Fahrenheit){$Fahrenheit=$True}
+        if($Celsius)
+            {
+            $c = $(($WmiBase)-273.15)
+            $Bucket += "$c" + [char]0x00B0 + " Celsius"
+            }
+        if($Fahrenheit)
+            {
+            $f = $((($WmiBase)-273.15) * (9/5) + 32)
+            $Bucket += "$f" + [char]0x00B0 +" Fahrenheit"
+            }
+        if($Kelvin)
+            {
+            $k = $WmiBase
+            $Bucket += "$k" + [char]0x00B0 +" Kelvin"
+            }
+        }
+    end
+       {return $Bucket}
+}
+
+<#
+.Synopsis
+Get-PassiveTripPoint uses the MSAcpi_ThermalZoneTemperature class to return the passive tripping point temperature of the cPU.
+
+.Parameter Celsius
+Return the passive tripping point as Celsius
+
+.Parameter Fahrenheit
+Return the passive tripping point as Fahrenheit
+
+.Parameter Kelvin
+Return the passive tripping point as Kelvin
+
+.Description
+Get-PassiveTripPoint uses the MSAcpi_ThermalZoneTemperature class to return the passive tripping point temperature of the cPU.  Specify temperature unit using the parameters -Celsius, -Fahrenheit, and/or -Kelvin.
+
+.Example
+Get-PassiveTripPoint -Celsius
+
+.Example
+Get-PassiveTripPoint -Fahrenheit
+
+.Example
+Get-PassiveTripPoint -Kelvin
+
+.Example
+Get-PassiveTripPoint -Celsius -Kelvin -Fahrenheit
+#>
+
+function Get-PassiveTripPoint 
+{
+    [CmdletBinding()]
+    param(
+        [Switch]$Celsius,
+        [Switch]$Fahrenheit,
+        [Switch]$Kelvin
+        )
+    begin 
+        {
+        $WmiBase = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+        $WmiBase = $($WmiBase.PassiveTripPoint/10)
+        $Bucket = @();
+        }
+    process
+        {
+        if(!$Celsius -and !$Kelvin -and !$Fahrenheit){$Fahrenheit=$True}
+        if($Celsius)
+            {
+            $c = $(($WmiBase)-273.15)
+            $Bucket += "$c" + [char]0x00B0 + " Celsius"
+            }
+        if($Fahrenheit)
+            {
+            $f = $((($WmiBase)-273.15) * (9/5) + 32)
+            $Bucket += "$f" + [char]0x00B0 +" Fahrenheit"
+            }
+        if($Kelvin)
+            {
+            $k = $WmiBase
+            $Bucket += "$k" + [char]0x00B0 +" Kelvin"
+            }
+        }
+    end
+       {return $Bucket}
+}
+
+<#
+.Synopsis
+Get-SamplingPeriod uses the MSAcpi_ThermalZoneTemperature class to return the sampling period.
+
+.Description
+Get-SamplingPeriod uses the MSAcpi_ThermalZoneTemperature class to return the sampling period.
+
+.Example
+Get-SamplingPeriod
+#>
+
+function Get-SamplingPeriod 
+{
+    begin 
+        {
+        $WmiBase = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+        $WmiBase = $WmiBase.SamplingPeriod
+        }
+    process
+        {}
+    end
+       {return $WmiBase}
+}
+
+<#
+.Synopsis
+Get-ThermalStamp uses the MSAcpi_ThermalZoneTemperature class to return the thermal stamp.
+
+.Description
+Get-ThermalStamp uses the MSAcpi_ThermalZoneTemperature class to return the thermal stamp.
+
+.Example
+Get-ThermalStamp
+#>
+
+function Get-ThermalStamp 
+{
+    begin 
+        {
+        $WmiBase = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+        $WmiBase = $WmiBase.ThermalStamp
+        }
+    process
+        {}
+    end
+       {return $WmiBase}
+}
+
+Get-CurrentCPUTemperature -Celsius -Kelvin -Fahrenheit
+Get-CriticalTripPoint -Celsius -Kelvin -Fahrenheit
+Get-PassiveTripPoint -Celsius -Kelvin -Fahrenheit
+Get-SamplingPeriod
+Get-ThermalStamp
+Export-ModuleMember -Cmdlet * -Function *
