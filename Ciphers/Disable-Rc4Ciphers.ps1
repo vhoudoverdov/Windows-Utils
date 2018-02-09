adb40b38de998253def3727b3e50da3bf909b2bc
Vasken Houdoverdov <vhcomptech@gmail.com>
2018-02-08 20:43:26 -0800

    tls utility

--- Ciphers/Disable-Rc4Ciphers.ps1
+++ Ciphers/Disable-Rc4Ciphers.ps1
@@ -6,16 +6,44 @@
      Disable-Rc4Ciphers
  #>  
  
- Function Disable-Rc4Ciphers {
+ Function Disable-Rc4Ciphers
+ {
  
- $Root = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
  
- New-Item -Path "$Root$('RC4 128')$([char]0x2215)128" -Force
- New-ItemProperty -Path "$Root$('RC4 128')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+    New-Item -Path "$Path$('RC4 128')$([char]0x2215)128" -Force
+    New-ItemProperty -path "$Path$('RC4 128')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
  
- New-Item -Path "$Root$('RC4 56')$([char]0x2215)128" -Force
- New-ItemProperty -Path "$Root$('RC4 56')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+    New-Item -Path "$Path$('RC4 56')$([char]0x2215)128" -Force
+    New-ItemProperty -path "$Path$('RC4 56')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
  
- New-Item -Path "$Root$('RC4 40')$([char]0x2215)128" -Force
- New-ItemProperty -Path "$Root$('RC4 40')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
- }
+    New-Item -Path "$Path$('RC4 40')$([char]0x2215)128" -Force
+    New-ItemProperty -path "$Path$('RC4 40')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+}
+
+
+Function Disable-Tls 
+{
+Param([Switch]$DisableVersion1_0,
+      [Switch]$DisableVersion1_1)
+   
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
+
+    if($DisableVersion1_0){
+    New-Item "$Path$('TLS 1.0\Server')" -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.0\Server')" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.0\Server')" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+
+    New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.0\Client')" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.0\Client')" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null}
+
+   if($DisableVersion1_1){
+    New-Item "$Path$('TLS 1.1\Server')" -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.1\Server')" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.1\Server')" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+   
+    New-Item "$Path$('TLS 1.1\Client')" -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.1\Client')" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+    New-ItemProperty -path "$Path$('TLS 1.1\Client')" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null}
+}
