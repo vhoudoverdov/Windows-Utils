d57a034e923f6bf04c3a1e4a6fd96fb893bc2606
Vasken Houdoverdov <vhcomptech@gmail.com>
2018-02-13 21:46:01 -0800

    cipher utils

--- CipherUtils/InsecureCipherUtils.psm1
+++ CipherUtils/InsecureCipherUtils.psm1
@@ -1,6 +1,6 @@
-<# 
+﻿<# 
  .Synopsis 
-     Disable insecure RC4 ciphers on Windows.  Disables RC4 128/128, RC4 56/128, and RC4 40/128  
+     Disable insecure RC4 ciphers on Windows.  Disables RC4 128/128, RC4 64/128, RC4 56/128, and RC4 40/128  
      Vasken Houdoverdov
  .Example
      Disable-Rc4Ciphers
@@ -14,6 +14,9 @@
     New-Item -Path "$Path$('RC4 128')$([char]0x2215)128" -Force
     New-ItemProperty -Path "$Path$('RC4 128')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
  
+    New-Item -Path "$Path$('RC4 64')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC4 64')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+
     New-Item -Path "$Path$('RC4 56')$([char]0x2215)128" -Force
     New-ItemProperty -Path "$Path$('RC4 56')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
  
@@ -23,23 +26,63 @@
 
 <# 
  .Synopsis 
+     Disable insecure RC2 ciphers on Windows.  Disables RC2 128/128, RC2 56/128, and RC2 40/128  
+     Vasken Houdoverdov
+ .Example
+     Disable-Rc2Ciphers
+ #>  
+ 
+ Function Disable-Rc2Ciphers
+ {
+ 
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
+ 
+    New-Item -Path "$Path$('RC2 128')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 128')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+
+    New-Item -Path "$Path$('RC2 56')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 56')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+ 
+    New-Item -Path "$Path$('RC2 40')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 40')$([char]0x2215)128" -PropertyType DWORD -Value '0' -Name 'Enabled'
+}
+
+<# 
+ .Synopsis 
+     Disable insecure DES ciphers on Windows.  Disables DES 56/56  
+     Vasken Houdoverdov
+ .Example
+     Disable-DesCiphers
+ #>  
+ 
+ Function Disable-DesCiphers
+ {
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
+ 
+    New-Item -Path "$Path$('DES 56')$([char]0x2215)56" -Force
+    New-ItemProperty -Path "$Path$('DES 56')$([char]0x2215)56" -PropertyType DWORD -Value '0' -Name 'Enabled'
+}
+
+<# 
+ .Synopsis 
      Disable insecure versions of TLS on Windows.  Disables TLS 1.0 and TLS 1.1  
      Vasken Houdoverdov
  .Example
-     Disable-Tls -DisableVersion1_0
+     Disable-Tls -DisableVersion 1.0
  .Example
-     Disable-Tls -DisableVersion1_1
+     Disable-Tls -DisableVersion 1.1
  .Example
-     Disable-Tls -DisableVersion1_0 -DisableVersion1_1    
+     Disable-Tls -DisableVersion Both 
  #> 
 Function Disable-Tls 
 {
-Param([Switch]$DisableVersion1_0,
-      [Switch]$DisableVersion1_1)
-   
+Param([Parameter(Mandatory=$true)]
+      [ValidateSet('1.0','1.1','Both')]
+      [String]$DisableVersion)
+ 
     $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
 
-    if($DisableVersion1_0){
+    if($DisableVersion -eq '1.0' -or $DisableVersion -eq 'Both'){
     New-Item "$Path$('TLS 1.0\Server')" -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Server')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Server')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force
@@ -48,7 +91,7 @@ Param([Switch]$DisableVersion1_0,
     New-ItemProperty -Path "$Path$('TLS 1.0\Client')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Client')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force}
 
-   if($DisableVersion1_1){
+    if($DisableVersion -eq '1.1' -or $DisableVersion -eq 'Both'){
     New-Item "$Path$('TLS 1.1\Server')" -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Server')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Server')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force
@@ -60,7 +103,45 @@ Param([Switch]$DisableVersion1_0,
 
 <# 
  .Synopsis 
-     Enable insecure RC4 ciphers on Windows.  Enables RC4 128/128, RC4 56/128, and RC4 40/128  
+     Disable insecure versions of SSL on Windows.  Disables SSL 2.0 and SSL 3.0
+     Vasken Houdoverdov
+ .Example
+     Disable-Ssl -DisableVersion 2.0
+ .Example
+     Disable-Ssl -DisableVersion 3.0
+ .Example
+     Disable-Ssl -DisableVersion Both
+ #> 
+Function Disable-Ssl 
+{
+Param([Parameter(Mandatory=$true)]
+      [ValidateSet('2.0','3.0','Both')]
+      [String]$DisableVersion)
+ 
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
+
+    if($DisableVersion -eq '2.0' -or $DisableVersion -eq 'Both'){
+    New-Item "$Path$('SSL 2.0\Server')" -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Server')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Server')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force
+
+    New-Item "$Path$('TLS 1.0\Client')" -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Client')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Client')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force}
+
+    if($DisableVersion -eq '3.0' -or $DisableVersion -eq 'Both'){
+    New-Item "$Path$('TLS 1.1\Server')" -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Server')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Server')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force
+   
+    New-Item "$Path$('TLS 1.1\Client')" -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Client')" -Name 'Enabled' -Value '0' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Client')" -Name 'DisabledByDefault' -Value 1 -PropertyType DWORD -Force}
+}
+
+<# 
+ .Synopsis 
+     Enable insecure RC4 ciphers on Windows.  Enables RC4 128/128, RC4 64/128, RC4 56/128, and RC4 40/128  
      Vasken Houdoverdov
  .Example
      Enable-Rc4Ciphers
@@ -74,11 +155,53 @@ Param([Switch]$DisableVersion1_0,
     New-Item -Path "$Path$('RC4 128')$([char]0x2215)128" -Force
     New-ItemProperty -Path "$Path$('RC4 128')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
  
+    New-Item -Path "$Path$('RC4 64')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC4 64')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
+ 
     New-Item -Path "$Path$('RC4 56')$([char]0x2215)128" -Force
     New-ItemProperty -Path "$Path$('RC4 56')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
  
     New-Item -Path "$Path$('RC4 40')$([char]0x2215)128" -Force
     New-ItemProperty -Path "$Path$('RC4 40')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
+}
+
+<# 
+ .Synopsis 
+     Enable insecure RC2 ciphers on Windows.  Enables RC2 128/128, RC2 56/128, and RC2 40/128  
+     Vasken Houdoverdov
+ .Example
+     Enable-Rc2Ciphers
+ #>  
+ 
+ Function Enable-Rc2Ciphers
+ {
+ 
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
+ 
+    New-Item -Path "$Path$('RC2 128')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 128')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
+ 
+    New-Item -Path "$Path$('RC2 56')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 56')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
+ 
+    New-Item -Path "$Path$('RC2 40')$([char]0x2215)128" -Force
+    New-ItemProperty -Path "$Path$('RC2 40')$([char]0x2215)128" -PropertyType DWORD -Value '1' -Name 'Enabled'
+}
+
+<# 
+ .Synopsis 
+     Enable insecure DES ciphers on Windows.  Enables DES 56/56
+     Vasken Houdoverdov
+ .Example
+     Enable-DesCiphers
+ #>  
+ 
+ Function Enable-DesCiphers
+ {
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\'
+ 
+    New-Item -Path "$Path$('DES 56')$([char]0x2215)56" -Force
+    New-ItemProperty -Path "$Path$('RC2 56')$([char]0x2215)56" -PropertyType DWORD -Value '1' -Name 'Enabled'
 }
 
 <# 
@@ -94,12 +217,13 @@ Param([Switch]$DisableVersion1_0,
  #> 
 Function Enable-Tls 
 {
-Param([Switch]$EnableVersion1_0,
-      [Switch]$EnableVersion1_1)
-   
+Param([Parameter(Mandatory=$true)]
+      [ValidateSet('1.0','1.1','Both')]
+      [String]$EnableVersion)
+ 
     $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
 
-    if($EnableVersion1_0){
+    if($EnableVersion -eq '1.0' -or $EnableVersion -eq 'Both'){
     New-Item "$Path$('TLS 1.0\Server')" -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Server')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Server')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' 
@@ -108,7 +232,7 @@ Param([Switch]$EnableVersion1_0,
     New-ItemProperty -Path "$Path$('TLS 1.0\Client')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.0\Client')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' }
 
-   if($EnableVersion1_1){
+    if($EnableVersion -eq '1.1' -or $EnableVersion -eq 'Both'){
     New-Item "$Path$('TLS 1.1\Server')" -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Server')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Server')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' 
@@ -116,6 +240,44 @@ Param([Switch]$EnableVersion1_0,
     New-Item "$Path$('TLS 1.1\Client')" -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Client')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
     New-ItemProperty -Path "$Path$('TLS 1.1\Client')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' }
+}
+
+<# 
+ .Synopsis 
+     Enable older versions of SSL on Windows.  Enables SSL 2.0 and SSL 3.0  
+     Vasken Houdoverdov
+ .Example
+     Enable-Ssl -EnableVersion 2.0
+ .Example
+     Enable-Ssl -EnableVersion 3.0
+ .Example
+     Enable-Ssl -EnableVersion Both
+ #> 
+Function Enable-Ssl 
+{
+Param([Parameter(Mandatory=$true)]
+      [ValidateSet('2.0','3.0','Both')]
+      [String]$EnableVersion)
+ 
+    $Path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
+
+    if($EnableVersion -eq '2.0' -or $EnableVersion -eq 'Both'){
+    New-Item "$Path$('SSL 2.0\Server')" -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Server')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Server')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' 
+
+    New-Item "$Path$('SSL 2.0\Server')" -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Client')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 2.0\Client')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' }
+
+    if($EnableVersion -eq '3.0' -or $EnableVersion -eq 'Both'){
+    New-Item "$Path$('TLS 1.1\Server')" -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Server')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Server')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' 
+   
+    New-Item "$Path$('TLS 1.1\Client')" -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Client')" -Name 'Enabled' -Value '1' -PropertyType DWORD -Force
+    New-ItemProperty -Path "$Path$('SSL 3.0\Client')"-PropertyType DWORD -Value 0  -Name 'DisabledByDefault' }
 }
 
 Export-ModuleMember *
